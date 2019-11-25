0c4d6b51fd9650cef867ee9e0bdcb5a9cb38da0c
charizard1ash <nathan.s.simonson@gmail.com>
2019-11-25 22:20:38 +1100

    Update model_performance.R

--- model_performance.R
+++ model_performance.R
@@ -17,7 +17,23 @@ model_performance <- function(model, predict, actual) {
                     })
   results <- rbindlist(l=results, use.names=TRUE, fill=TRUE)
   results[, names(results[, !c("model","class")]):=lapply(.SD, function(x) ifelse(is.nan(x) | is.na(x), 0, x)), .SDcols=names(results[, !c("model","class")])]
-  results_consolidated <- results[, .(model, accuracy=accuracy, precision, weighted_precision=precision*weight, recall, weighted_recall=recall*weight, f1_score, weighted_f1_score=f1_score*weight)]
-  results_consolidated <- results_consolidated[, .(accuracy=mean(accuracy), mean_precision=mean(precision), weighted_precision=sum(weighted_precision), mean_recall=mean(recall), weighted_recall=sum(weighted_recall), mean_f1_score=mean(f1_score), weighted_f1_score=sum(weighted_f1_score)), by=.(model)]
-  return(list("confusion_matrix"=confusion_matrix, "performance_results"=results, "results_consolidated"=results_consolidated))
+  results_consolidated <- results[, .(model,
+                                      accuracy=accuracy,
+                                      precision,
+                                      weighted_precision=precision*weight,
+                                      recall,
+                                      weighted_recall=recall*weight,
+                                      f1_score,
+                                      weighted_f1_score=f1_score*weight)]
+  results_consolidated <- results_consolidated[, .(accuracy=mean(accuracy),
+                                                   mean_precision=mean(precision),
+                                                   weighted_precision=sum(weighted_precision),
+                                                   mean_recall=mean(recall),
+                                                   weighted_recall=sum(weighted_recall),
+                                                   mean_f1_score=mean(f1_score),
+                                                   weighted_f1_score=sum(weighted_f1_score)),
+                                               by=.(model)]
+  return(list("confusion_matrix"=confusion_matrix,
+              "performance_results"=results,
+              "results_consolidated"=results_consolidated))
 }
