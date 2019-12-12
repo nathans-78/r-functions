b0023b335769c419478ba7aa978ea34ef184f0f6
charizard1ash <nathan.s.simonson@gmail.com>
2019-12-12 22:27:19 +1100

    Update permutation_importance.R

--- permutation_importance.R
+++ permutation_importance.R
@@ -32,13 +32,12 @@ permutation_importance <- function(dt, y, model, model_name, pred_func="predict(
                                            dt_2 <- data.table(model_name=model_name, iteration=x2, loss_func_diff=y_2_loss-y_loss)
                                            return(dt_2)
                                          })
+                        x_out2 <- rbindlist(l=x_out2, use.names=TRUE, fill=TRUE)
+                        return(x_out2)
                       } else if (class_0_1_prob==FALSE) { # for multi-class classification
-                        # unique y-class values
-                        y_class <- dt[, .N, get(y)][, get(y)][order(get(y))]
-                        
                         # log-loss for model
-                        dt_temp <- dt
-                        x_char <- sapply(dt, is.character)
+                        dt_temp <- dt # dummy temp table to replaced by original
+                        x_char <- sapply(dt, is.character) # transform character variables to factor, maintain order in model.matrix pivot
                         x_char <- names(x_char[x_char==TRUE])
                         dt[, c(x_char):=lapply(.SD, as.factor), .SDcols=x_char]
                         dt <- data.table(model.matrix(~., data=dt)[, -1])
@@ -52,16 +51,58 @@ permutation_importance <- function(dt, y, model, model_name, pred_func="predict(
                         y_loss <- -sum(log(y_loss_temp))
                         dt <- dt_temp # return original dataset
                         
-                        # Log-loss based on each randomised permutation for selected variable
+                        # log-loss based on each randomised permutation for selected variable
                         x_out2 <- lapply(1:n_perm,
                                          function(x2) {
                                            dt_2 <- dt
                                            dt_2[, (x1):=dt_2[sample(x=1:dt_2[, .N], size=dt_2[, .N], replace=FALSE), get(x1)]]
                                            dt_2 <- data.table(model.matrix(~., data=dt_2)[, -1])
+                                           y_2_prob <- eval(parse(text=gsub("=dt", "dt_2", pred_func)))
+                                           y_2_loss_temp <- cbind(data.table(y_prob), data.table(y_act=dt_2[, get(y)])) # output of probabilities per class
+                                           y_2_loss_temp <- sapply(1:dt_2[, .N], # iterate through each prediction and select only the output with their respective actual class
+                                                                   function(i) {
+                                                                     y_2_loss_vec <- y_2_loss_temp[i, get(y_act)]
+                                                                     return(y_2_loss_vec)
+                                                                   })
+                                           y_2_loss <- -sum(log(y_2_loss_temp))
+                                           dt_2 <- data.table(model_name=model_name, iteration=x2, loss_func_diff=y_2_loss-y_loss)
+                                           return(dt_2)
                                          })
+                        x_out2 <- rbindlist(l=x_out2, use.names=TRUE, fill=TRUE)
+                        return(x_out2)
                       } else {
                         return(NULL)
                       }
+                    } else if (loss=="sse") { # sums of squares output
+                      # sse for model
+                      dt_temp <- dt # dummy temp table to replaced by original
+                      x_char <- sapply(dt, is.character) # transform character variables to factor, maintain order in model.matrix pivot
+                      x_char <- names(x_char[x_char==TRUE])
+                      dt[, c(x_char):=lapply(.SD, as.factor), .SDcols=x_char]
+                      dt <- data.table(model.matrix(~., data=dt)[, -1])
+                      y_pred <- eval(parse(text=pred_func)) # regression predictions
+                      y_loss <- sum((dt[, get(y)]-y_pred)^2) # sse
+                      dt <- dt_temp # return original dataset
+                      
+                      # see based on each randomised permutation for each variable
+                      x_out2 <- sapply(1:n_perm,
+                                       function(x2) {
+                                         dt_2 <- dt
+                                         dt_2[, (x1):=dt_2[sample(x=1:dt_2[, .N], size=dt_2[, .N], replace=FALSE), get(x1)]]
+                                         dt_2 <- data.table(model.matrix(~., data=dt_2)[, -1])
+                                         y_2_pred <- eval(parse(text=gsub("=dt", "dt_2", pred_func)))
+                                         y_2_pred <- sum((dt_2[, get(y)]-y_2_pred)^2)
+                                         dt_2 <- data.table(model_name=model_name, iteration=x12, loss_func_diff=y_2_pred-y_pred)
+                                         return(dt_2)
+                                       })
+                      x_out2 <- rbindlist(l=x_out2, use.names=TRUE, fill=TRUE)
+                      return(x_out2)
+                    } else {
+                      return(NULL)
                     }
                   })
+  
+  # combine and return results
+  x_out1 <- rbindlist(l=x_out1, use.names=TRUE, fill=TRUE)
+  return(x_out1)
 }
